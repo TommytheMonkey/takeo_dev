1e3cea2bfe5969a73817a4cbe716b7aeda86c922
TommytheMonkey <142725684+TommytheMonkey@users.noreply.github.com>
2025-09-14 17:18:02 -0400

    added some shit in next config and package json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',   // tells Next to generate static HTML
 };
 
 export default nextConfig;
